d7568cda17b15dfa2f4ef1c05b80839990511cb2
Martin1248 <martinonlinedrive@gmail.com>
2018-12-13 12:41:39 +0100

    Initial iOS project commit

---
+++ GTD light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GTD light
+//
+//  Created by martinhuch on 13.12.18.
+//  Copyright © 2018 martin1248. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
