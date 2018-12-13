a5603dd4133dd08f7ea64a0c65dbef060603c28b
Martin1248 <martinonlinedrive@gmail.com>
2018-12-13 15:26:49 +0100

    Created custom class for second view controller

---
+++ GTD light/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  GTD light
+//
+//  Created by martinhuch on 13.12.18.
+//  Copyright © 2018 martin1248. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        textView.text = "New Note"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
